1b1f3b95dc8f61473565f47ab9413685a6203438
Taylous <taylous99@gmail.com>
2025-10-16 00:58:04 +0900

    initiate sandbox

---
+++ sandbox/src/App.tsx
@@ -0,0 +1,15 @@
+import { Box, TextField, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h1">Sandbox</Typography>
+
+      <Box>
+        <TextField />
+      </Box>
+    </div>
+  );
+}
+
+export default App;
